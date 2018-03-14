fea830da52b9d09aa31474a4677d794f3390b6e9
381510688 <381510688@qq.com>
2018-03-14 10:01:34 +0800

    文件批量上传Demo

--- web/src/views/vue/upload-filelist.vue
+++ web/src/views/vue/upload-filelist.vue
@@ -9,6 +9,7 @@
                 :on-preview="handlePreview"
                 :on-remove="handleRemove"
                 :file-list="fileList"
+                :data="data"
                 :auto-upload="false">
             <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
             <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
@@ -18,11 +19,16 @@
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
     name: '',
     data() {
       return {
-        fileList: []
+        fileList: [],
+        data: {
+          name: 'ligang',
+          age: 28
+        }
       }
     },
     methods: {
@@ -35,8 +41,40 @@
         this.fileList = fileList
         console.log(file, fileList);
       },
+
+      uploadFiles({uploadFiles, data, action, success, error}) {
+        let form = new FormData()
+        // 文件对象
+        uploadFiles.map(file => form.append("file", file.raw))
+        // 附件参数
+        for (let key in data) {
+          form.append(key, data[key])
+        }
+        let xhr = new XMLHttpRequest()
+        // 异步请求
+        xhr.open("post", action, true)
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4){
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
+              success && success(xhr.responseText)
+            } else {
+              error && error(xhr.status)
+            }
+          }
+        }
+        xhr.send(form)
+      },
+
       submitUpload() {
-        this.$refs.upload.submit();
+        console.log(this.$refs.upload)
+        let {uploadFiles, action, data} = this.$refs.upload
+        this.uploadFiles({
+          uploadFiles,
+          action,
+          data,
+          success: (response) => console.log('成功了', response),
+          error: (error) => console.log('失败了', error)
+        })
       },
       handleRemove(file, fileList) {
         console.log(file, fileList);
