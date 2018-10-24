b9e1adc181bb77579c684d19f0d23dc5252122c7
381510688 <381510688@qq.com>
2018-10-24 15:19:01 +0800

    docs(git hooks): hooks

--- git-hooks/sendEmail-post-receive.js
+++ git-hooks/sendEmail-post-receive.js
@@ -1,3 +1,30 @@
 /**
  * Created by ligang on 2018/10/24.
  */
+var nodemailer  = require('nodemailer');
+
+var mailTransport = nodemailer.createTransport({
+  host : 'smtp.qq.com',
+  secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
+  auth : {
+    user : '381510688@qq.com',
+    pass : 'ebjdmnnoeexubhfh'
+  },
+});
+
+var options = {
+  from: '李刚<381510688@qq.com>',
+  to: '381510688@qq.com,ligang@idss-cn.com',
+  // cc: ''  //抄送
+  subject: '一封来自Node Mailer的邮件',
+  text: '一封来自Node Mailer的邮件',
+  html: '<h1>好，这是一封来自NodeMailer的邮件！</h1><p><img src="cid:00000001"/></p>'
+};
+
+mailTransport.sendMail(options, function(err, msg){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(msg);
+  }
+});
