00d4b58dd77c76c2d5d771caa331a1c7d2808a2a
381510688 <381510688@qq.com>
2018-11-22 16:52:25 +0800

    1.txt

---
+++ vue-cli3-test/src/demo/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>hello component</p>
+    <button @click="getName">获取name</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'hello',
+    props: ['name'],
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      getName () {
+        // eslint-disable-next-line
+        console.log(this.name)
+      }
+    }
+  }
+</script>
